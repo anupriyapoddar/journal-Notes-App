7c2604bc886ab45b83d890d98c71e2faf427f5d9
Anupriya Poddar <anupriyapoddar2003@gmail.com>
2026-02-10 15:01:02 +0530

    Initial commit

---
+++ src/main/java/com/anupriyajava_/journalNotesApp/JournalNotes_Application.java
@@ -0,0 +1,13 @@
+package com.anupriyajava_.journalNotesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalNotes_Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalNotes_Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/anupriyajava_/journalNotesApp/JournalNotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupriyajava_.journalNotesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalNotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
